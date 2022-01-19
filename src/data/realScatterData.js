7f079a9ae54b2f5a42df7b319767d53979272397
Arya Boudaie <arya@aryaboudaie.com>
2022-01-19 12:11:59 -0800

    Add real data

---
+++ src/data/realScatterData.js
@@ -0,0 +1,419 @@
+const realData = {
+  "Ashkenazi Jew": {
+    "Persian Jew": [
+      {
+        phenotype: "Other non-epithelial cancer of skin",
+        pval: 2.56978691173335e-6,
+        coeff: 2.752364775791277,
+        cint: 1.1471601856520006,
+      },
+      {
+        phenotype: "Adjustment reaction",
+        pval: 1.249193308525494e-8,
+        coeff: -1.1086156642274467,
+        cint: 0.3816784227479366,
+      },
+      {
+        phenotype: "Asthma",
+        pval: 0.0002087613350452,
+        coeff: 0.9707551866318096,
+        cint: 0.513095268921442,
+      },
+      {
+        phenotype: "Nontoxic uninodular goiter",
+        pval: 7.36936422933958e-5,
+        coeff: -0.8457023857984218,
+        cint: 0.4181462803869615,
+      },
+      {
+        phenotype: "Pruritus and related conditions",
+        pval: 6.266378024195581e-5,
+        coeff: -0.8446255917735301,
+        cint: 0.4135954088381879,
+      },
+    ],
+    European: [
+      {
+        phenotype: "Alcoholic liver damage",
+        pval: 4.036543147486001e-7,
+        coeff: -1.5847580184628258,
+        cint: 0.6129721592191668,
+      },
+      {
+        phenotype: "Secondary thrombocytopenia",
+        pval: 0.0050861709263736,
+        coeff: -1.1234942394999925,
+        cint: 0.7860034416257773,
+      },
+      {
+        phenotype: "Lung transplant",
+        pval: 1.6733161711201068e-5,
+        coeff: -1.0550645248906292,
+        cint: 0.4803968726830865,
+      },
+      {
+        phenotype: "Heart transplant/surgery",
+        pval: 0.000725659697934,
+        coeff: -0.998029667736678,
+        cint: 0.5787804696312455,
+      },
+      {
+        phenotype: "Eating disorder",
+        pval: 3.16869979437741e-8,
+        coeff: 0.9945436529022116,
+        cint: 0.3523720952159735,
+      },
+      {
+        phenotype: "Liver abscess and sequelae of chronic liver disease",
+        pval: 8.207550766018814e-6,
+        coeff: -0.9925868968392272,
+        cint: 0.4362257034492878,
+      },
+      {
+        phenotype: "Cancer of tongue",
+        pval: 0.0014742422869327,
+        coeff: -0.9531775280797552,
+        cint: 0.5875364779894953,
+      },
+      {
+        phenotype: "Regional enteritis",
+        pval: 9.60040494831728e-15,
+        coeff: 0.8310406671869817,
+        cint: 0.2103198922739493,
+      },
+      {
+        phenotype: "Obsessive-compulsive disorders",
+        pval: 1.3576027079631167e-5,
+        coeff: 0.7843578082548588,
+        cint: 0.3533558725611101,
+      },
+      {
+        phenotype: "Alcohol-related disorders",
+        pval: 1.7819167939204332e-8,
+        coeff: -0.7453090737865856,
+        cint: 0.2593739349908727,
+      },
+      {
+        phenotype: "Malignant neoplasm of liver, primary",
+        pval: 0.0025728744707991,
+        coeff: -0.7342546210184078,
+        cint: 0.4773751776611923,
+      },
+      {
+        phenotype: "Disorders of mineral metabolism",
+        pval: 0.0030643755865777,
+        coeff: 0.7231387867114005,
+        cint: 0.4786314405356973,
+      },
+      {
+        phenotype: "Abdominal aortic aneurysm",
+        pval: 0.0014641404326542,
+        coeff: -0.7005759534418143,
+        cint: 0.4315630613973229,
+      },
+      {
+        phenotype: "Elevated sedimentation rate",
+        pval: 1.128280396973792e-5,
+        coeff: 0.6459621407422894,
+        cint: 0.288330936406439,
+      },
+      {
+        phenotype: "Inflammation of the eye",
+        pval: 0.0040845093571863,
+        coeff: 0.643743097696255,
+        cint: 0.4393824906662216,
+      },
+      {
+        phenotype: "Ulcerative colitis",
+        pval: 3.4500283532725363e-8,
+        coeff: 0.6142149296460193,
+        cint: 0.218208765647104,
+      },
+      {
+        phenotype: "Lymphoid leukemia, chronic",
+        pval: 0.001717910065719,
+        coeff: 0.6125465061009516,
+        cint: 0.382943487480733,
+      },
+      {
+        phenotype: "Anxiety, phobic and dissociative disorders",
+        pval: 0.0077530829863243,
+        coeff: 0.6026846981746568,
+        cint: 0.4436353778085941,
+      },
+      {
+        phenotype: "Degenerative disease of the spinal cord",
+        pval: 4.652636821442632e-5,
+        coeff: 0.5981806299014556,
+        cint: 0.2878905703939942,
+      },
+      {
+        phenotype: "Disturbances of sulphur-bearing amino-acid metabolism",
+        pval: 0.0078430283602304,
+        coeff: 0.5912024702483617,
+        cint: 0.4358190693316727,
+      },
+      {
+        phenotype: "Kidney replaced by transpant",
+        pval: 0.0009011477523746,
+        coeff: -0.582783696614973,
+        cint: 0.3440779701258517,
+      },
+      {
+        phenotype: "Ulcerative colitis (chronic)",
+        pval: 7.682801977184945e-6,
+        coeff: 0.5763692586328657,
+        cint: 0.2525042328475316,
+      },
+      {
+        phenotype:
+          "Disorders of refraction and accommodation; blindness and low vision",
+        pval: 0.0012456300781591,
+        coeff: 0.5723927271961984,
+        cint: 0.3475193846523638,
+      },
+      {
+        phenotype: "Astigmatism",
+        pval: 0.0063461702084546,
+        coeff: 0.5663903747361969,
+        cint: 0.406730462271409,
+      },
+      {
+        phenotype: "Pituitary hyperfunction",
+        pval: 0.0033440065207372,
+        coeff: 0.5555949818240461,
+        cint: 0.3711210548090609,
+      },
+      {
+        phenotype: "Psychogenic disorder",
+        pval: 0.0035244989702401,
+        coeff: 0.5454284155833666,
+        cint: 0.3663720781983667,
+      },
+      {
+        phenotype: "Essential tremor",
+        pval: 1.3992466060092282e-6,
+        coeff: 0.5399779311178414,
+        cint: 0.219339429207277,
+      },
+      {
+        phenotype: "Diseases of hair and hair follicles",
+        pval: 4.190427374913453e-5,
+        coeff: 0.5388822689085483,
+        cint: 0.2578134415024094,
+      },
+      {
+        phenotype: "Spinal stenosis",
+        pval: 1.078243343914257e-17,
+        coeff: 0.5090133709929616,
+        cint: 0.1164759610502553,
+      },
+      {
+        phenotype:
+          "Intestinal disaccharidase deficiencies and disaccharide malabsorption",
+        pval: 0.0002050907495991,
+        coeff: 0.5052361185869011,
+        cint: 0.2667209166167387,
+      },
+    ],
+    All: [
+      {
+        phenotype: "Viral hepatitis B",
+        pval: 3.442369973157571e-7,
+        coeff: -2.9603168404734523,
+        cint: 1.1382358677742426,
+      },
+      {
+        phenotype: "Alcoholic liver damage",
+        pval: 1.1221634798184332e-9,
+        coeff: -1.869377829603764,
+        cint: 0.6015302524699968,
+      },
+      {
+        phenotype: "Liver abscess and sequelae of chronic liver disease",
+        pval: 8.031673495337596e-13,
+        coeff: -1.5311498501659009,
+        cint: 0.4190979606561953,
+      },
+      {
+        phenotype: "Heart transplant/surgery",
+        pval: 2.627423331847843e-7,
+        coeff: -1.4617225039744663,
+        cint: 0.5564705963999093,
+      },
+      {
+        phenotype: "Malignant neoplasm of liver, primary",
+        pval: 6.993473399469242e-10,
+        coeff: -1.4227948478758337,
+        cint: 0.452241320100726,
+      },
+      {
+        phenotype: "Eating disorder",
+        pval: 2.4574639134550527e-14,
+        coeff: 1.2625893283345582,
+        cint: 0.3245799792706021,
+      },
+      {
+        phenotype: "Kidney replaced by transpant",
+        pval: 1.4070717589288086e-13,
+        coeff: -1.2228817066788435,
+        cint: 0.3240824980246886,
+      },
+      {
+        phenotype: "Obsessive-compulsive disorders",
+        pval: 1.5410778017170054e-11,
+        coeff: 1.158557597448929,
+        cint: 0.3367050028880058,
+      },
+      {
+        phenotype: "Lung transplant",
+        pval: 4.708591521899597e-6,
+        coeff: -1.0906762314143386,
+        cint: 0.4670119143899653,
+      },
+      {
+        phenotype: "Regional enteritis",
+        pval: 5.1014088004495486e-27,
+        coeff: 1.0823151301302507,
+        cint: 0.1970768589428594,
+      },
+      {
+        phenotype: "Renal dialysis",
+        pval: 4.440660149566329e-12,
+        coeff: -1.0729152003962923,
+        cint: 0.303778486357974,
+      },
+      {
+        phenotype: "Liver replaced by transplant",
+        pval: 4.5779769286190244e-8,
+        coeff: -1.0718977833616807,
+        cint: 0.3842863003108041,
+      },
+      {
+        phenotype: "Autism",
+        pval: 0.0010056454272191,
+        coeff: 1.0426539946501487,
+        cint: 0.6213438626627691,
+      },
+      {
+        phenotype: "Esophageal bleeding (varices/hemorrhage)",
+        pval: 4.957379912273146e-6,
+        coeff: -1.0124544234910302,
+        cint: 0.4345424264068573,
+      },
+      {
+        phenotype: "Chronic dermatitis due to solar radiation",
+        pval: 9.304127863252199e-11,
+        coeff: 0.9416021096758368,
+        cint: 0.2848950469575145,
+      },
+      {
+        phenotype: "Alopecia Areata",
+        pval: 0.0009962082139369,
+        coeff: 0.9025241287967884,
+        cint: 0.5374034692076309,
+      },
+      {
+        phenotype: "Portal hypertension",
+        pval: 2.6899917134035138e-6,
+        coeff: -0.8812749771621405,
+        cint: 0.3680382193360577,
+      },
+      {
+        phenotype: "Lymphoid leukemia, chronic",
+        pval: 5.350106098146537e-6,
+        coeff: 0.8394966545142084,
+        cint: 0.3615774730247492,
+      },
+      {
+        phenotype: "Cancer of liver and intrahepatic bile duct",
+        pval: 0.0004384704659008,
+        coeff: -0.8273535940111747,
+        cint: 0.461230512631001,
+      },
+      {
+        phenotype: "Juvenile rheumatoid arthritis",
+        pval: 0.0058655481166146,
+        coeff: 0.8215484158796258,
+        cint: 0.5844231783241842,
+      },
+      {
+        phenotype: "Anxiety, phobic and dissociative disorders",
+        pval: 0.0001306775628799,
+        coeff: 0.8132207343690139,
+        cint: 0.416682124283901,
+      },
+      {
+        phenotype: "Ulcerative colitis",
+        pval: 1.6383315326692076e-14,
+        coeff: 0.8000758986230327,
+        cint: 0.2042824454236831,
+      },
+      {
+        phenotype: "Cancer of tongue",
+        pval: 0.0078941372308061,
+        coeff: -0.7781161459744513,
+        cint: 0.574079673589576,
+      },
+      {
+        phenotype: "Hallux rigidus",
+        pval: 0.0020567594730917,
+        coeff: 0.7619290406112595,
+        cint: 0.4845542961528684,
+      },
+      {
+        phenotype: "Disturbances of sulphur-bearing amino-acid metabolism",
+        pval: 0.00028438336047,
+        coeff: 0.756303492457745,
+        cint: 0.4084531526315042,
+      },
+      {
+        phenotype: "Pervasive developmental disorders",
+        pval: 1.1198133349308483e-9,
+        coeff: 0.7534024813699673,
+        cint: 0.2424172422081663,
+      },
+      {
+        phenotype: "Laxity of ligament or hypermobility syndrome",
+        pval: 0.0003463104511558,
+        coeff: 0.7502633677409546,
+        cint: 0.4109874434233143,
+      },
+      {
+        phenotype: "Ulcerative colitis (chronic)",
+        pval: 1.355970751359929e-9,
+        coeff: 0.7289151085592903,
+        cint: 0.2357262369256192,
+      },
+      {
+        phenotype: "Attention deficit hyperactivity disorder",
+        pval: 2.0473626773000235e-10,
+        coeff: 0.7216703684617719,
+        cint: 0.2224763361597346,
+      },
+      {
+        phenotype: "Benign neoplasm of eye, uveal",
+        pval: 0.0104632787366348,
+        coeff: 0.7113504450001245,
+        cint: 0.5445898611608124,
+      },
+    ],
+  },
+};
+
+const generateOptions = (data) => {
+  const keys = Object.keys(data);
+  let options = [];
+  options = options.concat(keys);
+  keys.forEach(k => {
+    options = options.concat(Object.keys(data[k]))
+  });
+  options = [...new Set(options)];
+  options = options.sort();
+  options = options.filter(item => item !== 'All')
+  return options;
+}
+
+const realDataOptions = generateOptions(realData);
+
+export { realData, realDataOptions };
