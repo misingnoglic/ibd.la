beb83f668cd069abed12bc679f12d4b800364d1f
christa caggiano <christacaggiano@gmail.com>
2022-03-30 22:53:29 -0700

    Rejigger home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,53 +2,76 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
-import sharingGraph from "../data/images/sharingGraph.png";
+import louvain_graph from "../data/images/louvain_graph2.png";
 
-import "./HomePage.css";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
       <div className="titleText">
-        <Typography variant="h4">
+        <Typography variant="h2">
           Fine-scale population health in Los Angeles{" "}
         </Typography>
       </div>
       <Typography variant="h6"></Typography>
-      <img width="1000" height="600" src={sharingGraph}></img>
-      <div className="homeBodyText"><Typography variant="body1" gutterBottom>
-        <p>This browser explores population health in Los Angeles within the UCLA health system. The data is 
-        apart of the <Link href="https://www.uclahealth.org/precision-health/atlas">
-        UCLA ATLAS Community Health Initiative{" "}.</Link> Here, we explore several aspects of health that 
-        may be of interest to researchers and community members. </p> 
+      <div className="homePageContent">
+        <div className="graphImage">
+          <img height="100%" width="100%" src={louvain_graph}></img>
+        </div>
+        <div className="homeBodyText">
+          <Typography variant="body1" gutterBottom>
+            <p>
+              This browser explores population health in Los Angeles within the
+              UCLA health system. The data is apart of the{" "}
+              <Link href="https://www.uclahealth.org/precision-health/atlas">
+                UCLA ATLAS Community Health Initiative .
+              </Link>{" "}
+              Here, we explore several aspects of health that may be of interest
+              to researchers and community members.{" "}
+            </p>
 
-        <p>We first identify fine-scale populations using an unsupervised machine learning approach 
-        over genotyping data that is a part of the ATLAS intiative. This found hundreds of clusters of patients. 
-        Here, we focus on the 21 largest clusters, representing 97% of our sample. ATLAS data is connected to electronic 
-        health record (EHR) data. We use the EHR data to learn about who might be making up the clusters.
-        This data involved demographic data, such as self-reported race and ethnicity, preferred language, and preferred religion. 
-        </p>
+            <p>
+              We first identify fine-scale populations using an unsupervised
+              machine learning approach over genotyping data that is a part of
+              the ATLAS intiative. This found hundreds of clusters of patients.
+              Here, we focus on the 21 largest clusters, representing 97% of our
+              sample. ATLAS data is connected to electronic health record (EHR)
+              data. We use the EHR data to learn about who might be making up
+              the clusters. This data involved demographic data, such as
+              self-reported race and ethnicity, preferred language, and
+              preferred religion.
+            </p>
 
-        <p>We then explore how each community interacts with the health system. For this analysis, we considered 
-        what diagnoses, in the form of PheCodes, were more likley to be recieved by a cluster, and what medical specialties 
-        the community was more or less likely to visit. We also examined whether there were Los Angeles zipcodes where members 
-        of a community were more likely to visit a doctor's office. Lastly, we examined properties of genetic sharing within the 
-        community, something that might be useful to know about when studying the population's genetic risk for disease. 
-        </p>
+            <p>
+              We then explore how each community interacts with the health
+              system. For this analysis, we considered what diagnoses, in the
+              form of PheCodes, were more likley to be recieved by a cluster,
+              and what medical specialties the community was more or less likely
+              to visit. We also examined whether there were Los Angeles zipcodes
+              where members of a community were more likely to visit a doctor's
+              office. Lastly, we examined properties of genetic sharing within
+              the community, something that might be useful to know about when
+              studying the population's genetic risk for disease.
+            </p>
 
-        <p><b>Note</b> that the results presented here are not evidence of genetic causes for disease. Health is a complex 
-        phenomenon that is strongly influenced by environmental and sociocultural factors. The explicit goal of this research
-        is to advance the study of health disparities, especially for groups understudied in biomedical research.
-        Please see our FAQ page for a full discussion of what these results do or do not mean. 
-        </p>
+            <p>
+              <b>Note</b> that the results presented here are not evidence of
+              genetic causes for disease. Health is a complex phenomenon that is
+              strongly influenced by environmental and sociocultural factors.
+              The explicit goal of this research is to advance the study of
+              health disparities, especially for groups understudied in
+              biomedical research. Please see our FAQ page for a full discussion
+              of what these results do or do not mean.
+            </p>
 
-
-
-
-
-        <p>Website developed by Christa Caggiano, Arya Boudaie, and Noah Zaitlen. </p>
-
-      </Typography></div>
+            <p>
+              Website developed by Christa Caggiano, Arya Boudaie, and Noah
+              Zaitlen.
+            </p>
+          </Typography>
+        </div>
+      </div>
     </div>
   );
 };
