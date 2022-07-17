584533f443ea66dc29e73e669392c7fceedd3b3f
Arya Boudaie <arya@aryaboudaie.com>
2022-07-16 19:45:29 -0700

    Remove references to atlas

--- src/components/DeckGlMap.jsx
+++ src/components/DeckGlMap.jsx
@@ -67,7 +67,7 @@ const DeckGlMap = () => {
   const layers = [
     new GeoJsonLayer({
       id: "geojson",
-      data: `https://raw.githubusercontent.com/misingnoglic/atlas-app/main/data/geojson/${group}.json`,
+      data: `https://raw.githubusercontent.com/misingnoglic/ibd-la/main/data/geojson/${group}.json`,
       getFillColor: (f) => colorScale(f.properties.coefficient),
       getLineColor: [255, 255, 255],
       getLineWidth: 20,
