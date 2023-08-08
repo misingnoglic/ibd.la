a1800750c5e6352da041fc7e95740164648ebf90
Arya Boudaie <arya@aryaboudaie.com>
2023-08-07 17:04:05 -0700

    PRS page is now on the template system

--- src/components/PrsPage.jsx
+++ src/components/PrsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ScatterPlot from "./ScatterPlot";
 import { simulatedPrsData } from "../data/prs/simulatedPrs";
+import GraphPage from "./GraphPage/GraphPage";
 
 import Divider from "@mui/material/Divider";
 import InputLabel from "@mui/material/InputLabel";
@@ -47,59 +48,39 @@ const PrsPage = () => {
   ));
 
   return (
-    <div className={css.scatterplotBox}>
-      <Typography variant="h2">BETA PRS PAGE - SIMULATED DATA</Typography>
-      <Typography variant="h5">
-        Group={group} | Covariate={covariate}
-      </Typography>
-      <div className={css.scatterGraph}>{graph}</div>
-      <div className={css.selectionForm}>
-        <div className={css.selectionBox}>
-          <FormControl>
-            <InputLabel id="group1-selection">Cluster 1</InputLabel>
-            <Select
-              labelId="group1-selection"
-              value={group}
-              label="Group"
-              onChange={(e) => setGroup(e.target.value)}
-            >
-              {groupOptions}
-            </Select>
-          </FormControl>
-        </div>
-        <div className={css.selectionBox}>
-          <FormControl>
-            <InputLabel id="group2-selection">Cluster 2</InputLabel>
-            <Select
-              labelId="group2-selection"
-              value={covariate}
-              label="Covariate"
-              onChange={(e) => setCovariate(e.target.value)}
-            >
-              {covariateOptions}
-            </Select>
-          </FormControl>
-        </div>
-      </div>
-      <div className={css.bodyText2}>
-        <div className={css.sectionHeader}>
-          <Divider textAlign="left">
-            <Typography variant="h4">Model</Typography>
-          </Divider>
-        </div>
-        <Typography variant="body1" gutterBottom>
-          TODO
-        </Typography>
-        <div className={css.sectionHeader}>
-          <Divider textAlign="left">
-            <Typography variant="h4">About</Typography>
-          </Divider>
-        </div>
-        <Typography variant="body1" gutterBottom>
-          TODO
-        </Typography>
-      </div>
-    </div>
+    <GraphPage
+      title="BETA PRS PAGE - SIMULATED DATA"
+      subtitle={`${group} | ${covariate}`}
+      graph={graph}
+      graphControls={[
+        <FormControl>
+          <InputLabel id="group1-selection">Cluster 1</InputLabel>
+          <Select
+            labelId="group1-selection"
+            value={group}
+            label="Group"
+            onChange={(e) => setGroup(e.target.value)}
+          >
+            {groupOptions}
+          </Select>
+        </FormControl>,
+        <FormControl>
+          <InputLabel id="group2-selection">Cluster 2</InputLabel>
+          <Select
+            labelId="group2-selection"
+            value={covariate}
+            label="Covariate"
+            onChange={(e) => setCovariate(e.target.value)}
+          >
+            {covariateOptions}
+          </Select>
+        </FormControl>,
+      ]}
+      textSections={[
+        { title: "Model", content: "TODO" },
+        { title: "About", content: "TODO" },
+      ]}
+    />
   );
 };
 
